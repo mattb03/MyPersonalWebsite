9f152852480c9989cb77a6d5831b893d99dbde99
Matt <matt.benavides92@gmail.com>
2019-06-05 19:09:49 -0500

    Finished mounted() function. Added resizeMobile(), resizeTablet() and resizeDesktop() helper functions. Still need to do resizeTablet() and resizeDesktop().

--- src/components/HomeHero.vue
+++ src/components/HomeHero.vue
@@ -7,9 +7,9 @@
             <h2 class="subtitle animated fadeIn delay-2s" id="introSubtitle">
                 I'm a software engineer.
             </h2>
-            <a href="#AboutSection"> <!-- TODO: give this <a> an id, use it for margin-top instead of <i> element -->
-                <span class="icon">
-                    <i class="fas fa-angle-down animated fadeInUp delay-3s" id="arrowIcon"></i>
+            <a href="#AboutSection" id="arrowHref"> <!-- TODO: give this <a> an id, use it for margin-top instead of <i> element -->
+                <span class="icon" id="arrowIcon">
+                    <i class="fas fa-angle-down animated fadeInUp delay-3s"></i>
                 </span>
             </a>
         </div>
@@ -22,34 +22,92 @@
 
 export default {
     name: 'HomeHero',
+    data: function() {
+        return {
+            viewportWidth: window.innerWidth || document.documentElement.clientWidth,
+            orientation: screen.msOrientation || screen.mozOrientation || (screen.orientation || {}).type
+        }
+    },
     methods: {
         handleResize: function() {
             var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
-            //var orientation = screen.msOrientation || screen.mozOrientation || (screen.orientation || {}).type;
-            // need to finish this function, copy and use the conditions from mounted()
+            if (viewportWidth > 900) {
+                // desktop
+            } else {
+                if (viewportWidth > 700) {
+                    // ipad/tablet portrait
+                } else {
+                    // mobile 
+                    this.resizeMobile();
+                }
+            }
+        },
+        resizeMobile: function() {
+                var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+                var orientation = screen.msOrientation || screen.mozOrientation || (screen.orientation || {}).type;
+                document.getElementById('helloTitle').classList.remove('is-1');
+                document.getElementById('introSubtitle').classList.remove('is-2');
+                document.getElementById('arrowIcon').classList.remove('fa-4x');
+
+
+                document.getElementById('helloTitle').classList.add('is-3');
+                document.getElementById('introSubtitle').classList.add('is-4');
+                document.getElementById('arrowIcon').classList.add('fa-2x');
+
+                if (viewportWidth < 400) {
+                    document.getElementById('arrowIcon').classList.remove('icon-landscape-small');
+                    document.getElementById('arrowIcon').classList.add('icon-portrait-small');
+
+                    document.getElementById('sectionHero').classList.remove('hero-large');
+                    document.getElementById('sectionHero').classList.add('hero-small');
+                } else {
+                    document.getElementById('arrowIcon').classList.remove('icon-portrait-small');
+                    document.getElementById('arrowIcon').classList.add('icon-landscape-small');
+
+                    document.getElementById('sectionHero').classList.remove('hero-small');
+                    document.getElementById('sectionHero').classList.add('hero-large');
+                    
+                }
+        },
+        resizeTablet: function() {
+
+        },
+        resizeDesktop: function() {
+
         }
     },
     mounted() {
         var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
         var orientation = screen.msOrientation || screen.mozOrientation || (screen.orientation || {}).type;
-        if (viewportWidth > 800) {
+        if (viewportWidth > 900) {
+            document.getElementById('helloTitle').classList.add('is-1');
+            document.getElementById('introSubtitle').classList.add('is-2');
+            document.getElementById('arrowIcon').classList.add('fa-4x');
+            document.getElementById('arrowIcon').classList.add('icon-default');
+            document.getElementById('sectionHero').classList.add('hero-large');
+        } else {
+            // small screen
+            if (viewportWidth > 700) {
+                // ipad/tablet portrait
                 document.getElementById('helloTitle').classList.add('is-1');
                 document.getElementById('introSubtitle').classList.add('is-2');
                 document.getElementById('arrowIcon').classList.add('fa-4x');
-            if (orientation === 'landscape-primary' || orientation === 'landscape-secondary') {
                 document.getElementById('arrowIcon').classList.add('icon-default');
-                document.getElementById('sectionHero').classList.add('hero-large');
+                document.getElementById('sectionHero').classList.add('hero-small');
             } else {
-                document.getElementById('arrowIcon').classList.add('icon-portrait-small');
-                document.getElementById('sectionHero').classList.add('hero-portrait-small');
-            }
+                // mobile potrait/landscape
+                document.getElementById('helloTitle').classList.add('is-3');
+                document.getElementById('introSubtitle').classList.add('is-4');
+                document.getElementById('arrowIcon').classList.add('fa-2x');
 
-        } else {
-            document.getElementById('helloTitle').classList.add('is-3');
-            document.getElementById('introSubtitle').classList.add('is-4');
-            document.getElementById('arrowIcon').classList.add('fa-2x');
-            document.getElementById('arrowIcon').classList.add('icon-portrait-small');
-            document.getElementById('sectionHero').classList.add('hero-portrait-small');
+                if (viewportWidth < 400) {
+                    document.getElementById('arrowIcon').classList.add('icon-portrait-small');
+                    document.getElementById('sectionHero').classList.add('hero-small');
+                } else {
+                    document.getElementById('arrowIcon').classList.add('icon-landscape-small');
+                    document.getElementById('sectionHero').classList.add('hero-large');
+                }            
+            }
         }
         window.addEventListener('resize', this.handleResize);
     }
@@ -80,21 +138,17 @@ export default {
     background: url('../assets/mountain.jpeg');
     background-size: cover;
 }
-@media screen and (max-width: 900px) and (orientation:portrait) {
-    .hero-portrait-small {
-        height: 100vh;
-        background: url('../assets/mountaincropped.jpeg');
-        background-size: cover;
-    }
-    .icon-portrait-small {
-        color: darkgrey;
-        margin-top: 130%;
-    }
+.hero-small {
+    height: 100vh;
+    background: url('../assets/mountaincropped.jpeg');
+    background-size: cover;
 }
-@media screen and (max-width: 700px) and (orientation:landscape) {
-    .icon-landscape-small {
-        color: darkgrey;
-        margin-top: 25%;
-    }
+.icon-portrait-small {
+    color: darkgrey;
+    margin-top: 130%;
+}
+.icon-landscape-small {
+    color: darkgrey;
+    margin-top: 25%;
 }
 </style>
