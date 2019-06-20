eb31534ae67cc38a747b88ea71ceec3bba5d6222
Matt <matt.benavides92@gmail.com>
2019-06-19 21:18:48 -0500

    Added code to animate the fields in Contact section.

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,11 +1,11 @@
 <template>
     <section class="section hero">
-        <div class="container">
+        <div class="container" id="contactContainer">
             <h3 class="subtitle is-3">Contact Me</h3>
             <!-- TODO: figure out how to align the name and email boxes with the message box -->
             <div class="columns is-centered">
                 <div class="column">
-                    <div class="field">
+                    <div class="field animated fadeInUp delay-1s" id="nameField" hidden>
                         <label class="label">Name</label>
                         <div class="control">
                             <input type="text" class="input" placeholder="Enter name">
@@ -13,7 +13,7 @@
                     </div>
                 </div>
                 <div class="column">
-                    <div class="field">
+                    <div class="field animated fadeInUp delay-1s" id="emailField" hidden>
                         <label class="label">Email</label>
                         <div class="control">
                             <input type="text" class="input" placeholder="Enter email">
@@ -23,7 +23,7 @@
             </div>
             <div class="columns is-centered">
                 <div class="column">
-                    <div class="field">
+                    <div class="field animated fadeInUp delay-1s" id="messageField" hidden>
                         <label class="label">Message</label>
                         <div class="control">
                             <textarea cols="30" rows="10" class="textarea">Enter a message</textarea>
@@ -33,7 +33,7 @@
             </div>
             <div class="columns is-centered">
                 <div class="column is-one-third">
-                    <div class="field">
+                    <div class="field animated fadeInUp delay-2s" id="submitButtonField" hidden>
                         <div class="control">
                             <button class="button is-rounded" type="submit">Submit</button>
                         </div>
@@ -48,9 +48,20 @@
 export default {
     name: 'Contact',
     methods: {
+        handleScroll: function() {
+            
+            var rect = document.getElementById('contactContainer').getBoundingClientRect();
+            if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+                document.getElementById('nameField').removeAttribute('hidden');
+                document.getElementById('emailField').removeAttribute('hidden');
+                document.getElementById('messageField').removeAttribute('hidden');
+                document.getElementById('submitButtonField').removeAttribute('hidden');
+            }
 
+        }
     },
     mounted() {
+        document.addEventListener('scroll', this.handleScroll);
     }
 };
 </script>
