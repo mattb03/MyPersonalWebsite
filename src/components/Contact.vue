64c558abdd2d2c66365dafab7dfd68e0e1b160c8
Matt <matt.benavides92@gmail.com>
2019-06-20 16:27:20 -0500

    POST requested json payload from client side.

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -3,45 +3,43 @@
         <div class="container" id="contactContainer">
             <h3 class="subtitle is-3">Contact Me</h3>
             <!-- TODO: figure out how to align the name and email boxes with the message box -->
-            <form action="http://localhost:5000/sendMessage" method="POST">
-                <div class="columns is-centered">
-                    <div class="column">
-                        <div class="field animated fadeInUp delay-1s" id="nameField" hidden>
-                            <label class="label">Name</label>
-                            <div class="control">
-                                <input type="text" class="input" placeholder="Enter name">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="column">
-                        <div class="field animated fadeInUp delay-1s" id="emailField" hidden>
-                            <label class="label">Email</label>
-                            <div class="control">
-                                <input type="text" class="input" placeholder="Enter email">
-                            </div>
+            <div class="columns is-centered">
+                <div class="column">
+                    <div class="field animated fadeInUp delay-1s" id="nameField" hidden>
+                        <label class="label">Name</label>
+                        <div class="control">
+                            <input type="text" class="input" placeholder="Enter name" id="nameInput">
                         </div>
                     </div>
                 </div>
-                <div class="columns is-centered">
-                    <div class="column">
-                        <div class="field animated fadeInUp delay-1s" id="messageField" hidden>
-                            <label class="label">Message</label>
-                            <div class="control">
-                                <textarea cols="30" rows="10" class="textarea">Enter a message</textarea>
-                            </div>
+                <div class="column">
+                    <div class="field animated fadeInUp delay-1s" id="emailField" hidden>
+                        <label class="label">Email</label>
+                        <div class="control">
+                            <input type="text" class="input" placeholder="Enter email" id="emailInput">
                         </div>
                     </div>
                 </div>
-                <div class="columns is-centered">
-                    <div class="column is-one-third">
-                        <div class="field animated fadeInUp delay-2s" id="submitButtonField" hidden>
-                            <div class="control">
-                                <button class="button is-rounded" type="submit">Submit</button>
-                            </div>
+            </div>
+            <div class="columns is-centered">
+                <div class="column">
+                    <div class="field animated fadeInUp delay-1s" id="messageField" hidden>
+                        <label class="label">Message</label>
+                        <div class="control">
+                            <textarea cols="30" rows="10" class="textarea" id="messageTextarea">Enter a message</textarea>
                         </div>
                     </div>
                 </div>
-            </form>
+            </div>
+            <div class="columns is-centered">
+                <div class="column is-one-third">
+                    <div class="field animated fadeInUp delay-2s" id="submitButtonField" hidden>
+                        <div class="control">
+                            <button class="button is-rounded" id="submitButton">Submit</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 </template>
@@ -60,10 +58,38 @@ export default {
                 document.getElementById('submitButtonField').removeAttribute('hidden');
             }
 
+        },
+        handleSubmitButtonClick: function() {
+            var nameInput = document.getElementById('nameInput');
+            var emailInput = document.getElementById('emailInput');
+            var messageTextarea = document.getElementById('messageTextarea');
+            if (!nameInput.value) {
+                alert('Enter a name');
+                return;
+            }
+            if (!emailInput.value) {
+                alert('Enter an email');
+                return;
+            }
+            if (!messageTextarea.value) {
+                alert('Enter a message');
+                return;
+            }
+            fetch('http://localhost:5000/sendMessage', {
+                method: 'POST',
+                mode: 'cors',
+                body: JSON.stringify({
+                    'name': nameInput.value,
+                    'email': emailInput.value,
+                    'message': messageTextarea.value
+                })
+            })
+            .then(response => alert(response.status));
         }
     },
     mounted() {
         document.addEventListener('scroll', this.handleScroll);
+        document.getElementById('submitButton').addEventListener('click', this.handleSubmitButtonClick);
     }
 };
 </script>
