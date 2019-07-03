b9847593e09fdd5d55d00c95eaaf45bd2c2d5ed3
Matt <matt.benavides92@gmail.com>
2019-07-03 16:15:15 -0500

    see diff.

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -2,7 +2,7 @@
     <section class="section hero">
         <div class="container" id="contactContainer">
             <h3 class="subtitle is-3">Contact Me</h3>
-            <form name="contact" data-netlify="true" netlify>
+            <form name="contact" data-netlify="true">
                   <input type="hidden" name="form-name" value="contact" />  
                 <div class="columns is-centered">
                     <div class="column">
