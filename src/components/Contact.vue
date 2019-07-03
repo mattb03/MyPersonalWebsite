d4e9474685c812aa4506bb24d8bf3cd4f503f2e7
Matt <matt.benavides92@gmail.com>
2019-07-03 16:02:11 -0500

    see diff.

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -2,7 +2,7 @@
     <section class="section hero">
         <div class="container" id="contactContainer">
             <h3 class="subtitle is-3">Contact Me</h3>
-            <form data-netlify="true" netlify="true" method="POST" >
+            <form netlify>
                 <div class="columns is-centered">
                     <div class="column">
                         <div class="field animated fadeInUp delay-1s" id="nameField" hidden>
